b3ffcba7655dcf585ae7aefa5263752f25831ad4
bundlepak <37006080+bundlepak@users.noreply.github.com>
2018-03-04 18:52:21 +0200

    clinkz fix

--- clinkz.lua
+++ clinkz.lua
@@ -6,7 +6,15 @@ function clinkz.OnProjectile(projectile)
 	local myHero = Heroes.GetLocal()
 	if not myHero then return end
 	if projectile.source ~= myHero then return end
-    if NPC.GetUnitName(myHero) ~= "npc_dota_hero_clinkz" then return end
+	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_clinkz" then return end
+	source = projectile.source
+	target = projectile.target
+
+	clinkz.Combo(myHero, source, target)
+end
+
+function clinkz.Combo(myHero, source, target)
+	if NPC.HasModifier(source, "modifier_clinkz_wind_walk") then return end
 	local myMana = NPC.GetMana(myHero)
 
 	local Medallion  = NPC.GetItem(myHero, "item_medallion_of_courage", true)
@@ -21,24 +29,24 @@ function clinkz.OnProjectile(projectile)
 	local Diffus = NPC.GetItem(myHero, "item_diffusal_blade", true)
 	local nullif = NPC.GetItem(myHero, "item_nullifier", true)	 
 	
-			if NPC.IsEntityInRange(projectile.target, myHero, 700) and not NPC.HasModifier(projectile.target, "modifier_item_lotus_orb") and not NPC.HasState(projectile.target, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) then
-				if Medallion and Ability.IsCastable(Medallion, 0) then Ability.CastTarget(Medallion,projectile.target) end
-				if SolarCrest and Ability.IsCastable(SolarCrest, 0) then Ability.CastTarget(SolarCrest,projectile.target) end
-				if NPC.IsLinkensProtected(projectile.target) then
-					if Diffus and Ability.IsCastable(Diffus, myMana) then Ability.CastTarget(Diffus, projectile.target); end
-					if RoA and Ability.IsCastable(RoA, myMana) then Ability.CastTarget(RoA, projectile.target); end
-					if Halberd and Ability.IsCastable(Halberd, myMana) then Ability.CastTarget(Halberd, projectile.target); end
-					if hurrican and Ability.IsCastable(hurrican, myMana) then Ability.CastTarget(hurrican, projectile.target); end
-					if ForceStaff and Ability.IsCastable(ForceStaff, myMana) then Ability.CastTarget(ForceStaff, projectile.target); end
-					if Hex and Ability.IsCastable(Hex, myMana - 100) and Orchid and Ability.IsCastable(Orchid, myMana) then Ability.CastTarget(Orchid,projectile.target) end
-					if Hex and Ability.IsCastable(Hex, myMana - 100) and BloodThorn and Ability.IsCastable(BloodThorn, myMana) then Ability.CastTarget(BloodThorn,projectile.target) end
+			if NPC.IsEntityInRange(target, myHero, 700) and not NPC.HasModifier(target, "modifier_item_lotus_orb") and not NPC.HasState(target, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) then
+				if Medallion and Ability.IsCastable(Medallion, 0) then Ability.CastTarget(Medallion,target) end
+				if SolarCrest and Ability.IsCastable(SolarCrest, 0) then Ability.CastTarget(SolarCrest,target) end
+				if NPC.IsLinkensProtected(target) then
+					if Diffus and Ability.IsCastable(Diffus, myMana) then Ability.CastTarget(Diffus, target); end
+					if RoA and Ability.IsCastable(RoA, myMana) then Ability.CastTarget(RoA, target); end
+					if Halberd and Ability.IsCastable(Halberd, myMana) then Ability.CastTarget(Halberd, target); end
+					if hurrican and Ability.IsCastable(hurrican, myMana) then Ability.CastTarget(hurrican, target); end
+					if ForceStaff and Ability.IsCastable(ForceStaff, myMana) then Ability.CastTarget(ForceStaff, target); end
+					if Hex and Ability.IsCastable(Hex, myMana - 100) and Orchid and Ability.IsCastable(Orchid, myMana) then Ability.CastTarget(Orchid,target) end
+					if Hex and Ability.IsCastable(Hex, myMana - 100) and BloodThorn and Ability.IsCastable(BloodThorn, myMana) then Ability.CastTarget(BloodThorn,target) end
 				end
-				if not NPC.IsLinkensProtected(projectile.target) then
-					if Hex and Ability.IsCastable(Hex, myMana) then Ability.CastTarget(Hex, projectile.target); end
-					if Orchid and Ability.IsCastable(Orchid, myMana) then Ability.CastTarget(Orchid,projectile.target) end
-					if BloodThorn and Ability.IsCastable(BloodThorn, myMana) then Ability.CastTarget(BloodThorn,projectile.target) end
-					if nullif and Ability.IsCastable(nullif, myMana) then Ability.CastTarget(nullif, projectile.target); end
-					if RoA and Ability.IsCastable(RoA, myMana) then Ability.CastTarget(RoA, projectile.target); end
+				if not NPC.IsLinkensProtected(target) then
+					if Hex and Ability.IsCastable(Hex, myMana) then Ability.CastTarget(Hex, target); end
+					if Orchid and Ability.IsCastable(Orchid, myMana) then Ability.CastTarget(Orchid,target) end
+					if BloodThorn and Ability.IsCastable(BloodThorn, myMana) then Ability.CastTarget(BloodThorn,target) end
+					if nullif and Ability.IsCastable(nullif, myMana) then Ability.CastTarget(nullif, target); end
+					if RoA and Ability.IsCastable(RoA, myMana) then Ability.CastTarget(RoA, target); end
 				end
 			end
 end
