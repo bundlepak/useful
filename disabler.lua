89d3a7b91b19b0da8a998ba86c4ebdfd9b1038e4
bundlepak <37006080+bundlepak@users.noreply.github.com>
2018-03-04 02:28:12 +0200

    disabler fix

--- disabler.lua
+++ disabler.lua
@@ -13,27 +13,27 @@ function disabler.OnUpdate()
         local sameTeam = Entity.GetTeamNum(enemy) == myTeam
         if not sameTeam and not NPC.IsDormant(enemy) and Entity.GetHealth(enemy) > 0 then
             local dagger = NPC.GetItem(enemy,"item_blink")
-            if dagger and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(dagger) > 2 and Ability.SecondsSinceLastUse(dagger)<=1 and Ability.SecondsSinceLastUse(dagger)>0 then
+            if dagger and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(dagger) > 4 and Ability.SecondsSinceLastUse(dagger)<=1 and Ability.SecondsSinceLastUse(dagger)>0 then
                 disabler.Disable(myHero, enemy)
             end 
             local ck = NPC.GetAbility(enemy, "chaos_knight_reality_rift")
-            if ck and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(ck) > 2 and Ability.SecondsSinceLastUse(ck)<=1 and Ability.SecondsSinceLastUse(ck)>0 then
+            if ck and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(ck) > 4 and Ability.SecondsSinceLastUse(ck)<=1 and Ability.SecondsSinceLastUse(ck)>0 then
                 disabler.Disable(myHero, enemy)
             end
             local void = NPC.GetAbility(enemy, "faceless_void_time_walk")
-            if void and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(void) > 2 and Ability.SecondsSinceLastUse(void)<=1 and Ability.SecondsSinceLastUse(void)>0 then
+            if void and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(void) > 4 and Ability.SecondsSinceLastUse(void)<=1 and Ability.SecondsSinceLastUse(void)>0 then
                 disabler.Disable(myHero, enemy)
             end
             local pa = NPC.GetAbility(enemy, "phantom_assassin_phantom_strike")
-            if pa and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(pa) > 2 and Ability.SecondsSinceLastUse(pa)<=1 and Ability.SecondsSinceLastUse(pa)>0 then
+            if pa and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(pa) > 4 and Ability.SecondsSinceLastUse(pa)<=1 and Ability.SecondsSinceLastUse(pa)>0 then
                 disabler.Disable(myHero, enemy)
             end
             local slark = NPC.GetAbility(enemy, "slark_pounce")
-            if slark and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(slark) > 2 and Ability.SecondsSinceLastUse(slark)<=1 and Ability.SecondsSinceLastUse(slark)>0 then
+            if slark and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(slark) > 4 and Ability.SecondsSinceLastUse(slark)<=1 and Ability.SecondsSinceLastUse(slark)>0 then
                 disabler.Disable(myHero, enemy)
             end
             local qop = NPC.GetAbility(enemy, "queenofpain_blink")
-            if qop and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(qop) > 2 and Ability.SecondsSinceLastUse(qop)<=1 and Ability.SecondsSinceLastUse(qop)>0 then
+            if qop and NPC.IsEntityInRange(myHero, enemy, 700) and Ability.GetCooldownLength(qop) > 4 and Ability.SecondsSinceLastUse(qop)<=1 and Ability.SecondsSinceLastUse(qop)>0 then
                 disabler.Disable(myHero, enemy)
             end
             
